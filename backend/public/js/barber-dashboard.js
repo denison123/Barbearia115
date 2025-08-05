47e84ac1c29ccd55444a4c3b06ec518d4b1986d5
denison123 <denison2908@gmail.com>
2025-08-05 15:03:00 -0300

    atualização do barber-dashboard.js

--- backend/public/js/barber-dashboard.js
+++ backend/public/js/barber-dashboard.js
@@ -1,7 +1,7 @@
 // public/js/barber-dashboard.js
 
 // URL base do seu backend
-const API_BASE_URL = 'https://barbearia-backend-9h50.onrender.com/api';
+const API_BASE_URL = 'https://barbearia-backend-9h56.onrender.com';
 
 // Função para formatar datas para o formato YYYY-MM-DD
 function formatDate(date) {
