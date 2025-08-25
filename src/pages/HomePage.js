58ec4a508a14f75d945fd6d5a206577f6075f1fc
denison123 <denison2908@gmail.com>
2025-08-25 19:51:01 -0300

    atualização

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,13 +4,13 @@ import React from 'react';
 import Header from '../components/Header/Header';
 import HeroSection from '../components/HeroSection/HeroSection';
 import Footer from '../components/Footer/Footer';
+import './HomePage.css';
 
 const HomePage = () => {
   return (
-    <div>
+    <div className="home-page-container">
       <Header />
       <HeroSection />
-      {/* Aqui você pode adicionar outras seções, como a de serviços ou diferenciais */}
       <Footer />
     </div>
   );
