1b3e7b65a7a19c81e8fcc999138945380f6c9c17
denison123 <denison2908@gmail.com>
2025-08-26 09:54:10 -0300

    atualização

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import Header from '../components/Header/Header';
 import HeroSection from '../components/HeroSection/HeroSection';
-import Footer from '../components/Footer/Footer';
+//import Footer from '../components/Footer/Footer';
 import './HomePage.css';
 
 const HomePage = () => {
