dc34adc430b5a7641d30b7034b68a29605d722d6
denison123 <denison2908@gmail.com>
2025-08-26 10:17:59 -0300

    atualização

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+// src/components/Footer/Footer.js
+
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div>
+      <p>Seu conteúdo do Footer</p>
+    </div>
+  );
+};
+
+export default Footer;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,16 +1,16 @@
 // src/pages/HomePage.js
 
 import React from 'react';
-// import Header from '../components/Header/Header';
-// import HeroSection from '../components/HeroSection/HeroSection';
+import Header from '../components/Header/Header';
+import HeroSection from '../components/HeroSection/HeroSection';
 // import Footer from '../components/Footer/Footer';
 import './HomePage.css';
 
 const HomePage = () => {
   return (
     <div className="home-page-container">
-      {/* <Header /> */}
-      {/* <HeroSection /> */}
+      <Header />
+      <HeroSection /> 
       {/* <Footer /> */}
     </div>
   );
