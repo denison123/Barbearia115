382f1b143358bebe4266a45536919634ed85501e
denison123 <denison2908@gmail.com>
2025-08-26 10:06:29 -0300

    atualização

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,17 +1,17 @@
 // src/pages/HomePage.js
 
 import React from 'react';
-import Header from '../components/Header/Header';
+// import Header from '../components/Header/Header';
 // import HeroSection from '../components/HeroSection/HeroSection';
-import Footer from '../components/Footer/Footer';
+// import Footer from '../components/Footer/Footer';
 import './HomePage.css';
 
 const HomePage = () => {
   return (
     <div className="home-page-container">
-      <Header />
+      {/* <Header /> */}
       {/* <HeroSection /> */}
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 };
